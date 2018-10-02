b639e3746292a78fe47f99fb50189218ef90c2e5
armand <armand.foucault@telecom-bretagne.eu>
2018-10-02 10:38:55 +0200

    Adjusted screen abstraction's dimensions

--- engine/screen.py
+++ engine/screen.py
@@ -2,12 +2,25 @@ import pygame
 
 class Screen(pygame.Surface):
     def __init__(self, width, height, columns, rows):
-        super(width, height)
+        x_unit = width // columns
+        y_unit = height // rows
 
-        self.width = width
-        self.height = height
+        real_width = x_unit * columns
+        real_height = y_unit * rows
+
+        super(real_width, real_height)
+
+        self.width = real_width
+        self.height = real_height
         self.columns = columns
         self.rows = rows
+        self.x_unit = x_unit
+        self.y_unit = y_unit
 
     def blit(self, image, origin):
-        super().blit(image, origin)
+        """origin: pair within the (columns, rows) range"""
+        i, j = origin
+        x = i * self.x_unit
+        y = j * self.y_unit
+
+        super().blit(image, (x, y))
