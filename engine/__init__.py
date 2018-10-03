8c0c06ad7ef51c276098043f1b7b75df8eacdd65
armand <armand.foucault@telecom-bretagne.eu>
2018-10-03 11:29:38 +0200

    Fixed circular dependency between application and dialog

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,5 +1,6 @@
 from importlib import import_module
 
+application = import_module(".application", __name__)
 dialog = import_module(".dialog", __name__)
 screen = import_module(".screen", __name__)
 resources = import_module(".resources", __name__)
