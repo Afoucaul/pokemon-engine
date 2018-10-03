86ba6483d26744d55b538d3b5d07ba78f18c1a0d
armand <armand.foucault@telecom-bretagne.eu>
2018-10-03 10:16:11 +0200

    Delegated dialog management to Dialog class
    The Dialog class can be initialized, which lets it create its own grid
    for drawing frames and text

--- engine/dialog.py
+++ engine/dialog.py
@@ -1,35 +1,73 @@
+import pygame
+from pygame.locals import QUIT
 from .screen import Screen 
-from .resources import Dialog as R
+from .resources import DialogResources as R
 
 
-def dialog_box(text, target=None):
-    pass
+class Dialog:
+    window = None
+    screen = None
 
+    @classmethod
+    def init(cls, window, width=160, height=144, columns=20, rows=18):
+        cls.window = window
+        cls.screen = Screen(width, height, columns, rows)
 
-def simple_menu(options, target=None):
-    pass
+    @classmethod
+    def dialog_box(cls, text, half_grid=False):
+        """Display a dialog box on two lines at the bottom of the screen"""
+        target = cls.screen
 
+        x1 = target.columns - 1
+        y1 = target.rows - 1
+        x0 = 0
+        y0 = y1 - 6
+        if half_grid:
+            x0 //= 2
+            y0 //= 2
+            x1 //= 2
+            y1 //= 2
+    
+        print(x0, x1, y0, y1)
+        cls.draw_frame(x0, y0, x1, y1)
+        pygame.display.flip()
+    
+        run = True
+        while run:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    run = False
+    
+    @classmethod
+    def simple_menu(options, target=None):
+        pass
+    
+    @classmethod
+    def callback_menu(cls, options):
+        pass
+    
+    @classmethod
+    def draw_frame(cls, x0, y0, x1, y1):
+        """Draw a frame for a dialog box on the target screen"""
+        target = cls.screen
 
-def callback_menu(options, target=None):
-    pass
-
-
-def draw_frame(x0, y0, x1, y1, target=None):
-    """Draw a frame for a dialog box on the target screen"""
-    target.blit(R.frame('nw'), (x0, y0))
-    target.blit(R.frame('sw'), (x0, y1))
-    target.blit(R.frame('ne'), (x1, y0))
-    target.blit(R.frame('se'), (x1, y1))
-
-    for x in range(x0+1, x1):
-        target.blit(R.frame('n'), (x, y0))
-        target.blit(R.frame('s'), (x, y1))
-
-    for y in range(y0+1, y1):
-        target.blit(R.frame('w'), (x0, y))
-        print("Blitting W at ({}, {})".format(x0, y))
-        target.blit(R.frame('e'), (x1, y))
-
-    for x in range(x0+1, x1):
+        target.blit(R.frame('nw'), (x0, y0))
+        target.blit(R.frame('sw'), (x0, y1))
+        target.blit(R.frame('ne'), (x1, y0))
+        target.blit(R.frame('se'), (x1, y1))
+    
+        for x in range(x0+1, x1):
+            target.blit(R.frame('n'), (x, y0))
+            target.blit(R.frame('s'), (x, y1))
+    
         for y in range(y0+1, y1):
-            target.blit(R.background(), (x, y))
+            target.blit(R.frame('w'), (x0, y))
+            print("Blitting W at ({}, {})".format(x0, y))
+            target.blit(R.frame('e'), (x1, y))
+    
+        for x in range(x0+1, x1):
+            for y in range(y0+1, y1):
+                target.blit(R.background(), (x, y))
+
+        # cls.window.blit(target, (0, 0))
+        pygame.transform.scale(target, cls.window.get_size(), cls.window)
