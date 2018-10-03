cc3e73d92b1a5dd463580f0c8acba86471b580e6
armand <armand.foucault@telecom-bretagne.eu>
2018-10-03 11:24:30 +0200

    Wrote unit test for font loading

--- test/test_resources.py
+++ test/test_resources.py
@@ -18,3 +18,8 @@ def test_dialog_loading():
     for side in ['n', 's', 'e', 'w', 'ne', 'nw', 'se', 'sw', 'bg']:
         assert side in resources.DialogResources.frame_elements
         assert isinstance(resources.DialogResources.frame_elements[side], pygame.Surface)
+
+    resources.DialogResources.load_font_from_directory(os.path.join(RESOURCES_PATH, "font"))
+    for char in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ !?,.'":
+        assert char in resources.DialogResources.characters
+        assert isinstance(resources.DialogResources.characters[char], pygame.Surface)
