043367191eeecc97649ec07acfc1ddf356b642ba
armand <armand.foucault@telecom-bretagne.eu>
2018-10-12 18:49:52 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="pokemon_engine",
+    version="1.7",
+    packages=setuptools.find_packages(),
+    author='Armand Foucault',
+    author_email='armand.foucault@telecom-bretagne.eu',
+)
