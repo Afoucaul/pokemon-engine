2325bd1ea9c740a969fdac26166a7eb6272d0d2e
armand <armand.foucault@telecom-bretagne.eu>
2018-10-22 23:11:12 +0200

    Added disk path to world instances, and presenting height and width

--- engine/world.py
+++ engine/world.py
@@ -8,8 +8,17 @@ class World:
         self.collisions = np.zeros((height, width), dtype=np.int16)
         self.events = np.zeros((height, width), dtype=np.int16)
         self.npcs = []
+        self.path = ""
 
     def set_layer(self, layer_name, layer):
         print("Setting layer {} for world of size {}; new layer is of size {}".format(
             layer_name, self.lower_tiles.shape, (len(layer), len(layer[0]))))
         getattr(self, layer_name)[:] = layer
+
+    @property
+    def width(self):
+        return self.lower_tiles.shape[0]
+
+    @property
+    def height(self):
+        return self.lower_tiles.shape[1]
